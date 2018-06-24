f9c864cef998fdf569cf8b0859f06e69da6c397a
ToshieCosed <iquiera@gmail.com>
2018-06-23 17:05:05 -0700

    First Upload
    Official Fantasy CPU 8 bit  / 16 bit / 32 bit Register Add/Sub/ STA/ LDA
    / LD8, LD16, etc.

---
+++ Virtual Machine Assembler/Opcode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_Machine_Assembler
+{
+    public class Opcode
+    {
+        public enum paramtype{DEFAULT, REGISTER, VALUE, ADDRESS, REGISTERADDRESS};
+        public int param_amount;
+        public string memnonic;
+        public paramtype param1 = paramtype.DEFAULT;
+        public paramtype param2 = paramtype.DEFAULT;
+        public int byteindex = 0;
+
+        public paramtype get_param1() {
+            return param1;
+            }
+
+        public paramtype get_param2()
+        {
+            return param2;
+        }
+
+        public void setparam1(paramtype t)
+        {
+            param1 = t;
+        }
+
+        public void setparam2(paramtype t)
+        {
+            param2 = t;
+        }
+
+        public void setparamamount(int amount)
+        {
+            param_amount = amount;
+        }
+
+        public void setbyteindex(int index)
+        {
+            byteindex = index;
+        }
+    }
+}
---
+++ Virtual Machine Assembler/ValidParams.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_Machine_Assembler
+{
+    public class ValidParams
+    {
+
+        //RegisterAddressParam = new string[13];
+        //RegisterParams = new string[13];
+        public string[] RegisterAddressParams;
+        public string[] RegisterParams;
+
+        public void init()
+        {
+
+            this.RegisterAddressParams = init_RegisterAddressParams();
+            this.RegisterParams = init_RegisterParams();
+        }
+        public string[] init_RegisterAddressParams()
+        {
+            //RegisterAddressParam = new string[13];
+           
+            string[] RegisterAddressParams = { "$REGISTER0", "$REGISTER1", "$REGISTER2", "$REGISTER3", "$REGISTER4",
+                "$REGISTER5", "$REGISTER6", "$REGISTER7", "$REGISTER8", "$REGISTER9", "$REGISTER10", "$REGISTER11",
+                "$REGISTER12", "$REGISTER13" };
+            return RegisterAddressParams;
+
+        }
+
+        public string[] init_RegisterParams()
+        {
+             //RegisterParams = new string[13];
+            string[] RegisterParams = { "REGISTER0", "REGISTER1", "REGISTER2", "REGISTER3", "REGISTER4",
+                "REGISTER5", "REGISTER6", "REGISTER7", "REGISTER8", "REGISTER9", "REGISTER10","REGISTER11",
+                "REGISTER12", "REGISTER13" };
+            return RegisterParams;
+    }
+       
+
+
+        }
+
+}
+
+
+   
+
+
+
+
+
+
+
+
+
+
